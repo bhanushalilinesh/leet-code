a6aaa4b41c6eaa1b8f20bae4bd9f806d489ef742
Linesh Bhanushali <bhanushali.linesh@gmail.com>
2024-05-23 19:56:57 +0530

    1672. Richest Customer Wealth

---
+++ src/RichestCustomerWealth.java
@@ -0,0 +1,28 @@
+
+
+//1672. Richest Customer Wealth
+public class RichestCustomerWealth {
+    public static void main(String[] args) {
+        int[][] account = {{1,2,3},{3,2,1}};
+        System.out.println(maximumWealth(account));
+    }
+
+    /*
+    You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+     */
+    public static int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for (int[] account : accounts) {
+            int sum = 0;
+            for (int i : account) {
+                sum += i;
+            }
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
